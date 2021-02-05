7d8d53d9d462245cf6658239e32878273b39e035
Jay-flow <kimpetro153@gmail.com>
2021-02-05 14:56:31 +0900

    [Enhance] Changed project name high5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 function MyApp({ Component, pageProps }): JSX.Element {
+  const path = useRouter().pathname
+  useEffect(() => {
+    console.log(path)
+  }, [])
   return <Component {...pageProps} />
 }
 
